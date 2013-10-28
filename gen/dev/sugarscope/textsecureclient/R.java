86eae1d16379c991bdd9cbd8f5e692f60142bbaa
lanstat <lanstat@gmail.com>
2013-10-28 19:44:32 -0400

    2

--- gen/dev/sugarscope/textsecureclient/R.java
+++ gen/dev/sugarscope/textsecureclient/R.java
@@ -24,18 +24,30 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btnSend=0x7f080002;
+        public static final int edtPhone=0x7f080005;
+        public static final int edtSeed=0x7f080001;
+        public static final int listView1=0x7f080000;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_seed=0x7f030000;
+        public static final int activity_inbox=0x7f030000;
+        public static final int activity_seed=0x7f030001;
+        public static final int activity_sign_up=0x7f030002;
     }
     public static final class menu {
-        public static final int seed=0x7f070000;
+        public static final int inbox=0x7f070000;
+        public static final int seed=0x7f070001;
+        public static final int sign_up=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_inbox=0x7f050004;
+        public static final int title_activity_sign_up=0x7f050003;
     }
     public static final class style {
         /** 
